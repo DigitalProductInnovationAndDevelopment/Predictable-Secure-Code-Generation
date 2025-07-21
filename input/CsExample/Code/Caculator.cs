19aa6540b639cca1435e931afb9c1b3fc9cdaa5d
linfeng20001 <linfengguo01@gmail.com>
2025-07-22 00:29:19 +0200

    fix: CS Example finished + requirements

---
+++ input/CsExample/Code/Caculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code
+{
+     class Calculator
+    {
+       
+        public Calculator()
+        {
+            
+        }
+
+        // Methode zum Addieren
+        public double Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        // Methode zum Multiplizieren
+        public double Multiply(int x, int y)
+        {
+            return x * y;
+        }
+     }
+}
---
+++ input/CsExample/Code/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+
+namespace Code
+{
+    class programm
+    {
+        static void Main()
+        {
+            Calculator calculator = new Calculator();
+
+            calculator.Add(1,1);
+            calculator.Multiply(1,1);
+
+        }
+    }
+}
